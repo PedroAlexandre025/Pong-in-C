46334e156f3f971a2c456741076af5eedcd4d4c9
clebaoxd <pedro612007@gmail.com>
2026-08-22 13:01:24 -0300

    Pong-com-mudanças-no-render-dia-22/08-funcionand-por-completo

--- pong.c
+++ pong.c
@@ -9,13 +9,22 @@
 #define l 50 //largure
 #define initialBallX l/2
 #define initialBallY h/2
+#define PaddleTop 8
+
+typedef enum{
+    falso, truee
+}boolean;
 
 
-int PosBarPlayer1 = 8;
-int PosBarPlayer2 = 8;
+typedef struct{
+    int PaddleY;
+    int PaddleX; //
+    int tam;
+}Paddle;
 
 typedef struct{
     int points;
+    Paddle bar;
 }Player;
 
 typedef struct{
@@ -23,12 +32,23 @@ typedef struct{
     int ballPosY;
     int ballDirectionX;
     int ballDirectionY;
+    boolean moving;
+    
 
 }Ball;
 
-void createPlayer(Player *player){
+void createPlayer1(Player *player){
     player->points = 0;
-   
+    player->bar.PaddleX = 3;
+    player->bar.PaddleY = 8;
+    player->bar.tam = 3;
+}
+
+void createPlayer2(Player *player){
+    player->points = 0;
+    player->bar.PaddleX = l-4;
+    player->bar.PaddleY = 8;
+    player->bar.tam = 3;
 }
 
 void createBall(Ball *ball){
@@ -36,6 +56,7 @@ void createBall(Ball *ball){
     ball->ballPosY = initialBallY;
     ball->ballDirectionX = 1;
     ball->ballDirectionY = 1;
+    ball->moving = falso;
 }
 
 void setCampEdges(char camp[h][l]){ //set the edges of the camp with '*'
@@ -56,89 +77,64 @@ void setCampEdges(char camp[h][l]){ //set the edges of the camp with '*'
 
 }
 
-void movimentBarPlayer1(char camp[h][l], int actualPositrionbar1, int beforeValue){//set the position of the bars in the camp, after the player press 1 the keybind to move the bar
 
-    for (int i = 0; i < 3; i++)
-    {
-        camp[i+beforeValue][2] = ' ';
-       
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        camp[i+actualPositrionbar1][2] = '*';
-        
-    }
-}
-void movimentBarPlayer2(char camp[h][l], int actualPositrionbar2, int beforeValue){//set the position of the bars in the camp, after the player 2 press the keybind to move the bar
-
-    for (int i = 0; i < 3; i++)
-    {
-        camp[i+beforeValue][l-3] = ' ';
-
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        camp[i+actualPositrionbar2][l-3] = '*';
-    }
-    
-}
-
-void createCamp(char camp[h][l]){
+void createCamp(char camp[h][l], Player *p1, Player *p2, Ball *ball){
     
     setCampEdges(camp);//edges
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < p1->bar.tam; i++)
     {
-        camp[i+PosBarPlayer1][2] = '*';
+        camp[i+p1->bar.PaddleY][p1->bar.PaddleX] = '*';
     }                                       //bars                  
     
-     for (int i = 0; i < 3; i++)
+     for (int i = 0; i < p2->bar.tam; i++)
     {
-        camp[i+PosBarPlayer2][l-3] = '*';
+        camp[i+p2->bar.PaddleY][p2->bar.PaddleX] = '*';
 
     }
 
-    camp[initialBallY][initialBallX] = 'O';//ball
+    camp[ball->ballPosY][ball->ballPosX] = 'O';//ball
 
-
-  
 }   
 
+void pressingKeybind(char* keybind, Player *p1, Player *p2, Ball *ball){//function that will be called when the player press a keybind, and will move the bar of the player accordingly
 
-
-void pressingKeybind(char* keybind, char camp[h][l]){//function that will be called when the player press a keybind, and will move the bar of the player accordingly
+    *keybind = getchar();
     switch (*keybind)
     {
         
         case 'w':
-            if(PosBarPlayer1 > 1){//if the bar of player 1 is not at the  top of the camp, move it up
-                --PosBarPlayer1;
-                movimentBarPlayer1(camp, PosBarPlayer1, PosBarPlayer1+1);
+            if(p1->bar.PaddleY > 1){//if the bar of player 1 is not at the  top of the camp, move it up
+                --p1->bar.PaddleY;
+                ball->moving = truee;
+                
             }
 
         break;
         case 's':
-            if(PosBarPlayer1 < h-4){//  if the bar of player 1 is not at the bottom of the camp, move it down
-                ++PosBarPlayer1;
-                movimentBarPlayer1(camp, PosBarPlayer1, PosBarPlayer1-1);
-            }
-        
+            if(p1->bar.PaddleY < h-4){//  if the bar of player 1 is not at the bottom of the camp, move it down
+                ++p1->bar.PaddleY;
+                ball->moving = truee;
+              
+            }       
         break;
 
         case '8':
-            if(PosBarPlayer2 >1){//if the bar of player 2 is not at the bottom of the camp, move it down
-                --PosBarPlayer2;
-                movimentBarPlayer2(camp, PosBarPlayer2, PosBarPlayer2+1);
+            if(p2->bar.PaddleY>1){//if the bar of player 2 is not at the bottom of the camp, move it up
+                --p2->bar.PaddleY;
+                ball->moving = truee;
+                
             }
         break;
         case '2':
-            if(PosBarPlayer2 <h-4){//if the bar of player 2 is not at the top of the camp, move it up
-                ++PosBarPlayer2;
-                movimentBarPlayer2(camp, PosBarPlayer2, PosBarPlayer2-1);
+            if(p2->bar.PaddleY <h-4){//if the bar of player 2 is not at the top of the camp, move it down
+                ++p2->bar.PaddleY;
+                ball->moving = truee;
+               
             }
         break;
         default:
-            break;
+        break;
             
     }
 
@@ -167,83 +163,63 @@ void printCamp(char camp[h][l]){//function that will print the camp in the conso
     }
     
 }
-void movimentBall(char camp[h][l], Ball *ball, Player* p1, Player* p2 ){//function that will move the ball in the camp, and will check if the ball hit the bar or the wall
+void movimentBall( Ball *ball, Player* p1, Player* p2 ){//function that will move the ball in the camp, and will check if the ball hit the bar or the wall
     
-    int beforeX = ball->ballPosX;
-    int beforeY = ball->ballPosY;
-
     ball->ballPosX += ball->ballDirectionX;
     ball->ballPosY += ball->ballDirectionY;
     
-    if (ball->ballPosX == 0)
+    if (ball->ballPosX == 1)//if the ball hit the left wall, reset the ball position and add a point to player 2
     {
-        ball->ballPosX = initialBallX;
+        ball->ballPosX = initialBallX; 
         ball->ballPosY = initialBallY;
         p2->points+=1;
         ball->ballDirectionX *= -1;
+        ball->moving = falso;
         
-    
-    }else if(ball->ballPosX == l-1){
+    }else if(ball->ballPosX == l-2){ //if the ball hit the right wall, reset the ball position and add a point to player 1
         ball->ballPosX = initialBallX;
         ball->ballPosY = initialBallY;
         p1->points+=1;
         ball->ballDirectionX *= -1;
+        ball->moving = falso;
         
     }   
     
-   
-    
-    if(ball->ballPosY == 0 || ball->ballPosY == h-1){//if the ball hit the wall, change the direction of the ball
+    if(ball->ballPosY == 1 || ball->ballPosY == h-2){//if the ball hit the wall, change the direction of the ball
         ball->ballDirectionY *= -1;
     
     }
 
-
     for (int i = 0; i < 3; i++)//verify the three positions of bars
     {
-        int counterP1 = PosBarPlayer1 +i;
-        int counterP2 = PosBarPlayer2 +i;
-        if ((ball->ballPosX == 2 && ball->ballPosY == counterP1)|| ball->ballPosX == 47  && ball->ballPosY == counterP2)
+        int counterP1 = p1->bar.PaddleY +i;
+        int counterP2 = p2->bar.PaddleY+i;
+        if ((ball->ballPosX == 3 && ball->ballPosY == counterP1)|| (ball->ballPosX == 46  && ball->ballPosY == counterP2))
         {
             ball->ballDirectionX *= -1;
             break;
         }
     }
-    for (int i = 0; i < 3; i++)
-    {   int counterP1 = PosBarPlayer1+i;
-        int counterP2 = PosBarPlayer2+i;
-        if ((beforeY == counterP1 && beforeX == 2)||(beforeY == counterP2 && beforeX == 47))
-        {
-            camp[beforeY][beforeX] = '*';
-            break;
-        }
-        
-    }
-    
-    
-    
-    if ((beforeX == 49 || beforeX == 0) ||( beforeY == 19 || beforeY == 0)) //switch char in icon
-    {
-        camp[beforeY][beforeX] = '*';
-    }else{
-        camp[beforeY][beforeX] = ' ';
-    }
-        
-    
-    
-    camp[ball->ballPosY][ball->ballPosX] = 'O'; 
-    usleep(50000);
+
+    usleep(80000);
     //ball switch position
-    
+
 }
 
 void clearScreen(){//function that will clear the console screen
     #if defined(_WIN32)|| defined(_WIN64)
         system("cls");
-
     #else
         system("clear");
     #endif
+}
+
+void render(char camp[h][l], Ball *ball, Player *p1, Player *p2){
+    
+    createCamp(camp, p1, p2, ball);
+    clearScreen();
+    printCamp(camp);
+
 }
 
 
@@ -251,36 +227,41 @@ int main(){
 
     Ball ball;
     createBall(&ball);
-
     char camp[h][l] = {};
     memset(camp, ' ', sizeof(camp));
-
     char keybind;
-
     Player player1, player2;
-    createPlayer(&player1);
-    createPlayer(&player2);
-
-    createCamp(camp);
-    printCamp(camp);
-    
+    createPlayer1(&player1);
+    createPlayer2(&player2);
     noEnter();
-
+    boolean changed = falso;//boolean for verify if one keybind was pressed
+    render(camp, &ball, &player1, &player2);
     while((player1.points < 10 && player2.points < 10)){//while the player 1 and player 2 points are less than 10, the game will continue, and if the player press 0, the game will end
+        memset(camp, ' ', sizeof(camp));
+       
+        pressingKeybind(&keybind,&player1, &player2, &ball);  
+      
+        if(ball.moving == truee){ //the game start soleny someone the players input keybind
+            movimentBall(&ball, &player1, &player2);
+            changed = truee;
+        }
+
+        if (changed ==truee)//the render start soleny if the first condition is true
+        {
+            render(camp, &ball, &player1, &player2);
+            printf("\n\n\n  Player 1: %d                                                                                 Player 2: %d\n", player1.points, player2.points);
+            changed = falso;
+        }
         
-        keybind = getchar();
+       
 
-        pressingKeybind(&keybind, camp);
-          
-        movimentBall(camp, &ball, &player1, &player2);
+        if(player1.points == 10){
+            printf("\n\n\n  Player 1 wins!\n");
+            
+        }else if(player2.points == 10){ 
+           printf("\n\n\n  Player 2 wins!\n");
 
-        clearScreen();
-
-        printCamp(camp);
-
-        printf("\n\n\n  Player 1: %d                                                                                 Player 2: %d\n", player1.points, player2.points);
-
-
-    }
+        }
+    }   
     return 0;
 }
